5967a74941e421931218c18d56ab48b5e7812879
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-27 14:24:43 -0400

    completes all assignments except for #2 bc bug

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// Declare global variable customerName
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+// Define function to set global variable bestCustomer
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+  // Note: using the `window` object explicitly is not strictly necessary,
+  // but it makes it clear that we are creating a global variable.
+}
+
+// Define function to overwrite global variable bestCustomer
+function overwriteBestCustomer() {
+  window.bestCustomer = 'maybe bob';
+}
+
+// Declare constant leastFavoriteCustomer in global scope
+const leastFavoriteCustomer = 'John';
+
+// Define function to attempt to change leastFavoriteCustomer constant
+function changeLeastFavoriteCustomer() {
+  // This will throw an error in strict mode
+  leastFavoriteCustomer = 'Jane';
+}
